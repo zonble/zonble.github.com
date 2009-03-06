b3be1914e8f170f00965d98037e9b6c414f5887e
zonble <zonble@gmail.com>
2009-03-07 01:19:08 +0800

    Creates a simple page with iWeb.

---
+++ zonble@github_files/zonble@github.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20090307
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,409),url:'zonble%40github_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'zonble%40github_files/stroke_1.png'},{rect:new IWRect(5,-5,380,10),url:'zonble%40github_files/stroke_2.png'},{rect:new IWRect(385,-5,10,10),url:'zonble%40github_files/stroke_3.png'},{rect:new IWRect(385,5,10,409),url:'zonble%40github_files/stroke_4.png'},{rect:new IWRect(385,414,10,10),url:'zonble%40github_files/stroke_5.png'},{rect:new IWRect(5,414,380,10),url:'zonble%40github_files/stroke_6.png'},{rect:new IWRect(-5,414,10,10),url:'zonble%40github_files/stroke_7.png'}],new IWSize(390,419)),reflection_0:new IWReflection({opacity:0.50,offset:6.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('zonble@github_files/zonble@githubMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
